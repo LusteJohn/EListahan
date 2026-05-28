f28a6f3d12e4bd12849c3414f58907be67401bc6
LusteJohn <luste.johnmark19@gmail.com>
2026-05-28 11:02:43 +0800

    feat: update layout and add index redirect to login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,37 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "login",
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack initialRouteName="login">
+        <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen
+          name="modal"
+          options={{ presentation: "modal", title: "Modal" }}
+        />
         <Stack.Screen
           name="category-form"
-          options={{ presentation: 'modal', title: 'Category' }}
+          options={{ presentation: "modal", title: "Category" }}
         />
         <Stack.Screen
           name="product-form"
-          options={{ presentation: 'modal', title: 'Product' }}
+          options={{ presentation: "modal", title: "Product" }}
         />
       </Stack>
       <StatusBar style="auto" />
